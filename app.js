d80260263d60cb4998457798f0342bdd405395eb
Daniel Grad <daniel.g@kaholo.io>
2022-05-06 09:44:35 +0200

    Lint fixes

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
-const request = require('request');
+const request = require("request");
 
-function getVaultRequestOptions(params, settings){
-	// Get token from settings, and check it exists
-	const token = (settings.token || ``).trim(); 
-	if (!token){	
-		throw `Not given token`;
-	}
-	// Get item key from parameters, and check it exists
-	const itemKey = (params.itemKey || ``).trim();;
-	if (!itemKey){
-		throw `Not given item key`;
-	}
-	// Get namespace from parameters, and check it exists
+function getVaultRequestOptions(params, settings) {
+  // Get token from settings, and check it exists
+  const token = (settings.token || "").trim();
+  if (!token) {
+    throw new Error("Not given token");
+  }
+  // Get item key from parameters, and check it exists
+  const itemKey = (params.itemKey || "").trim();
+  if (!itemKey) {
+    throw new Error("Not given item key");
+  }
+  // Get namespace from parameters, and check it exists
 
-	const namespace = (params.namespace || ``).trim();;
-	if (namespace && !namespace.endsWith(`/`)){
-		namespace += `/`;
-	}
-	// Get vault url from settings and add the path to the item wanted
-	const vaultUrl = `${settings.vaultUrl || `http://127.0.0.1:8200`}/v1/${namespace}secret/data/${itemKey}`;
+  let namespace = (params.namespace || "").trim();
+  if (namespace && !namespace.endsWith("/")) {
+    namespace += "/";
+  }
+  // Get vault url from settings and add the path to the item wanted
+  const vaultUrl = `${settings.vaultUrl || "http://127.0.0.1:8200"}/v1/${namespace}secret/data/${itemKey}`;
 
-	return {
-        url : vaultUrl,
-        json : true,
-        auth : {
-			bearer: token,
-			sendImmediately : true
-		}
-    };
+  return {
+    url: vaultUrl,
+    json: true,
+    auth: {
+      bearer: token,
+      sendImmediately: true,
+    },
+  };
 }
 
-async function sendHttpReq(requestOptions){
-	return new Promise((resolve,reject)=>{
-        request(requestOptions,function (err, response, _) {
-            if(err){
-                return reject(err);
-            }
-            resolve(response.body);
-        });
+async function sendHttpReq(requestOptions) {
+  return new Promise((resolve, reject) => {
+    request(requestOptions, (err, response) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(response.body);
     });
+  });
 }
-async function getVaultItem(action, settings){
-	let requestOptions = getVaultRequestOptions(action.params, settings);
-	requestOptions.method = `GET`;
-	return sendHttpReq(requestOptions);
-}
-
-async function getSecret(action, settings){
-	const vaultItem = await getVaultItem(action, settings);
-	const dataItem = vaultItem.data.data;
-	if (dataItem.value){
-		return dataItem.value;
-	}
-	return dataItem;
+async function getVaultItem(action, settings) {
+  const requestOptions = getVaultRequestOptions(action.params, settings);
+  requestOptions.method = "GET";
+  return sendHttpReq(requestOptions);
 }
 
-async function setVaultItem(action, settings){
-	let requestOptions = getVaultRequestOptions(action.params, settings);
-	const itemVal = action.params.itemVal;
-	if (!itemVal){
-		throw "Not given value!"
-	}
-	requestOptions.method = `POST`;
-	requestOptions.body = {data: {value: itemVal}}
-	return sendHttpReq(requestOptions);
+async function getSecret(action, settings) {
+  const vaultItem = await getVaultItem(action, settings);
+  const dataItem = vaultItem.data.data;
+  if (dataItem.value) {
+    return dataItem.value;
+  }
+  return dataItem;
+}
+
+async function setVaultItem(action, settings) {
+  const requestOptions = getVaultRequestOptions(action.params, settings);
+  const { itemVal } = action.params;
+  if (!itemVal) {
+    throw new Error("Not given value!");
+  }
+  requestOptions.method = "POST";
+  requestOptions.body = { data: { value: itemVal } };
+  return sendHttpReq(requestOptions);
 }
 
 module.exports = {
-    getVaultItem,
-	getSecret,
-	setVaultItem
-}
+  getVaultItem,
+  getSecret,
+  setVaultItem,
+};
